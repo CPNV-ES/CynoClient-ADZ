f43698431fe083711235e7d9f796c26656fb756e
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2021-05-21 13:46:27 +0200

    Move unchecked cast to create method

--- app/src/main/java/ch/leytto/cynoclient/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/ch/leytto/cynoclient/viewmodel/ViewModelFactory.kt
@@ -8,13 +8,12 @@ import ch.leytto.cynoclient.model.DogRepository
 import ch.leytto.cynoclient.ui.dogs.DogViewModel
 
 class ViewModelFactory(private val repository: AbstractRepository) : ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(DogViewModel::class.java)) {
-                @Suppress("UNCHECKED_CAST")
                 return DogViewModel(repository as DogRepository) as T
             }
             if (modelClass.isAssignableFrom(ClientViewModel::class.java)) {
-                @Suppress("UNCHECKED_CAST")
                 return ClientViewModel(repository as ClientRepository) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
