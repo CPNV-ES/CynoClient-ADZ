11ef01496189e83fb6899591f601ac47bdd950be
zaidschouwey98 <zazar981@hotmail.com>
2021-06-02 16:02:56 +0200

    Create fragment createClient

--- app/src/main/java/ch/leytto/cynoclient/CreateClientActivity.kt
+++ app/src/main/java/ch/leytto/cynoclient/CreateClientActivity.kt
@@ -2,10 +2,40 @@ package ch.leytto.cynoclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.viewModels
+import ch.leytto.cynoclient.db.CynoClientRoomDatabase
+import ch.leytto.cynoclient.db.entities.Client
+import ch.leytto.cynoclient.viewmodel.ClientViewModel
+import ch.leytto.cynoclient.viewmodel.ViewModelFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class CreateClientActivity : AppCompatActivity() {
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { CynoClientRoomDatabase.getDatabase(this,applicationScope) }
+    val viewModel: ClientViewModel by viewModels {
+        ViewModelFactory((application as CynoClientApplication).clientRepository)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_client)
+        var et_firstName = findViewById<EditText>(R.id.editPersonFirstName)
+        var et_lastName = findViewById<EditText>(R.id.editPersonLastName)
+        var et_email = findViewById<EditText>(R.id.editPersonEmail)
+        var et_password = findViewById<EditText>(R.id.editPersonPassword)
+        var et_confirmPassword = findViewById<EditText>(R.id.editPersonPasswordConfirm)
+        var btn_submit = findViewById<Button>(R.id.submitForm)
+
+        btn_submit.setOnClickListener {
+            val firstname = et_firstName.text.toString()
+            val lastname = et_lastName.text.toString()
+            val email = et_email.text.toString()
+            //viewModel.insertClient(Client(0,firstname,lastname))
+            Toast.makeText(this@CreateClientActivity, "L'utilisateur "+et_firstName.text +" "+ et_lastName.text+" a bien été créé.",Toast.LENGTH_LONG).show()
+        }
     }
+
 }
--- app/src/main/java/ch/leytto/cynoclient/viewmodel/ClientViewModel.kt
+++ app/src/main/java/ch/leytto/cynoclient/viewmodel/ClientViewModel.kt
@@ -3,10 +3,17 @@ package ch.leytto.cynoclient.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import ch.leytto.cynoclient.db.entities.Client
 import ch.leytto.cynoclient.model.ClientRepository
+import kotlinx.coroutines.launch
 
 class ClientViewModel(private val repository: ClientRepository) : ViewModel() {
 
     val allClients: LiveData<List<Client>> = repository.allClients.asLiveData();
+    fun insertClient(client: Client){
+        viewModelScope.launch {
+            repository.insertClient(client)
+        }
+    }
 }
