cd8d4d80f9e0ba550f5c215ea78ce8e02b21c683
zaidschouwey98 <zazar981@hotmail.com>
2021-06-04 14:47:19 +0200

    Create_user

--- app/src/main/java/ch/leytto/cynoclient/CreateClientActivity.kt
+++ app/src/main/java/ch/leytto/cynoclient/CreateClientActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.activity.viewModels
 import ch.leytto.cynoclient.db.CynoClientRoomDatabase
+import ch.leytto.cynoclient.db.entities.Client
 import ch.leytto.cynoclient.viewmodel.ClientViewModel
 import ch.leytto.cynoclient.viewmodel.ViewModelFactory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import java.util.logging.Logger
 
 class CreateClientActivity : AppCompatActivity() {
     val applicationScope = CoroutineScope(SupervisorJob())
@@ -21,16 +24,34 @@ class CreateClientActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_client)
+        var sexe: Boolean
+        sexe = true
         var et_firstName = findViewById<EditText>(R.id.editFirstname)
         var et_lastName = findViewById<EditText>(R.id.editLastname)
         var et_email = findViewById<EditText>(R.id.editEmail)
+        var et_street = findViewById<EditText>(R.id.editStreet)
+        var et_locality = findViewById<EditText>(R.id.editLocality)
+        var et_phone = findViewById<EditText>(R.id.editPhone)
+        var rb_sexe = findViewById<RadioGroup>(R.id.rbGroupSex)
         var btn_submit = findViewById<Button>(R.id.submitForm)
+        rb_sexe.setOnCheckedChangeListener{group: RadioGroup?, checkedId: Int ->
+            if (checkedId === R.id.rbFemale) {
+                sexe = false
+            }else if (checkedId === R.id.rbMale){
+                sexe = true
+            }else{
+                sexe = false
+            }
 
+        }
         btn_submit.setOnClickListener {
             val firstname = et_firstName.text.toString()
             val lastname = et_lastName.text.toString()
             val email = et_email.text.toString()
-            //viewModel.insertClient(Client(0,firstname,lastname))
+            var street = et_street.text.toString()
+            var locality = et_locality.text.toString()
+            var phone = et_phone.text.toString()
+            viewModel.insertClient(Client(0,firstname,lastname,sexe,email,phone,street,locality.toInt()))
             Toast.makeText(this@CreateClientActivity, "L'utilisateur "+et_firstName.text +" "+ et_lastName.text+" a bien été créé.",Toast.LENGTH_LONG).show()
         }
     }
